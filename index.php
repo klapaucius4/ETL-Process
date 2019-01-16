15402785e670edcc392f13b551c5a845ea04ccd6
klapaucius4 <klapaucius.mk@gmail.com>
2019-01-16 23:07:17 +0100

    16.01 #simple modifications

--- index.php
+++ index.php
@@ -14,6 +14,11 @@
             <li>Agnieszka Chlebda</li>
             <li>Dawid Łysiak</li>
           </ul>
+          <p class="mt-5">
+            <a href="https://github.com/klapaucius4/ETL-Process" target="_blank" title="Repozytorium w serwisie Github">
+              <i class="fab fa-github-square fa-4x"></i>
+            </a>
+          </p>
         </div>
       </section>
 
@@ -21,52 +26,17 @@
 
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="documentation">
         <div class="my-auto">
-          <h2 class="mb-5">Dokumentacja</h2>
+          <h2 class="mb-5">Dokumentacja techniczna</h2>
 
           <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto">
               <h3 class="mb-5">Do pobrania</h3>
               <!-- <div class="subheading mb-3">Intelitec Solutions</div> -->
-              <a href="dokumentacja.pdf" class="btn btn-primary" download>Dokumentacja</a>
+              <a href="Dokumentacja-techniczna.pdf" class="btn btn-primary" download>Dokumentacja techniczna</a>
               <!-- <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p> -->
             </div>
-            <!-- <div class="resume-date text-md-right">
-              <span class="text-primary">March 2013 - Present</span>
-            </div> -->
           </div>
 
-          <!-- <div class="resume-item d-flex flex-column flex-md-row mb-5">
-            <div class="resume-content mr-auto">
-              <h3 class="mb-0">Web Developer</h3>
-              <div class="subheading mb-3">Intelitec Solutions</div>
-              <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
-            </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">December 2011 - March 2013</span>
-            </div>
-          </div>
-
-          <div class="resume-item d-flex flex-column flex-md-row mb-5">
-            <div class="resume-content mr-auto">
-              <h3 class="mb-0">Junior Web Designer</h3>
-              <div class="subheading mb-3">Shout! Media Productions</div>
-              <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
-            </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">July 2010 - December 2011</span>
-            </div>
-          </div>
-
-          <div class="resume-item d-flex flex-column flex-md-row">
-            <div class="resume-content mr-auto">
-              <h3 class="mb-0">Web Design Intern</h3>
-              <div class="subheading mb-3">Shout! Media Productions</div>
-              <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-            </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">September 2008 - June 2010</span>
-            </div>
-          </div> -->
 
         </div>
 
@@ -81,7 +51,7 @@
           <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto">
               <h3 class="mb-5">Do pobrania</h3>
-              <a href="instrukcja.pdf" class="btn btn-success" download>Instrukcja</a>
+              <a href="Instrukcja-obslugi.pdf" class="btn btn-success" download>Instrukcja obsługi</a>
             </div>
           </div>
 
